57c5179a857166f331dc1363278d7b5bc203cd40
devi <253954067+codesboiler@users.noreply.github.com>
2026-01-11 21:26:44 +0530

    Day 0: Environment setup and first C++ program

---
+++ Day00/intro.cpp
@@ -0,0 +1,15 @@
+// // Day 0 🚀
+
+// Didn’t start DSA today.
+// Used the day to set up my Instagram & GitHub
+// to stay consistent and accountable.
+
+// Real progress starts tomorrow.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "DSA from Scratch - Day 0" << endl;
+    return 0;
+}
